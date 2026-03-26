e08846976f4c0e22c38db6c410afaf87fbc0a76a
robertmercea <robertmercea@gmail.com>
2026-03-26 13:19:34 +0200

    lab04: min: Add solution
    Implemented the solution by comparing the values in the two registers,
    swapping them only if necessary and printing them out

--- labs/lab-04/tasks/min/support/min.asm
+++ labs/lab-04/tasks/min/support/min.asm
@@ -22,7 +22,13 @@ main:
     PRINTF64 `%d\n\x0`, r10 ; print the second number
                             ; DO NOT REMOVE/MODIFY THIS LINE
 
-    ; TODO: find the minimum of the two numbers and store it in r9
+		; to use: cmp, jmp and xchg
+		cmp r9, r10
+		jl print
+		xchg r9, r10
+
+print:
+
     PRINTF64 `%d\n\x0`, r9 ; print the minimum
     xor rax, rax
 
