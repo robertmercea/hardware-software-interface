948c47717b2e587cae7ff84644e10eb70990c29b
robertmercea <robertmercea@gmail.com>
2026-03-26 13:04:56 +0200

    lab04: conditional-jumps: Add solution
    Finished the exercise through a comparison with 0 and a jump instruction

--- labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
+++ labs/lab-04/tasks/conditional-jumps/support/hello_world.asm
@@ -4,6 +4,7 @@
 
 section .data
     myString: db "Hello, World!", 0
+		byeString: db "Goodbye, World!", 0
     N: dq 6                         ; N = 6
 
 section .text
@@ -20,7 +21,8 @@ main:
     mov rax, 2
     mov r8, 1
     cmp rax, r8
-    je print                        ; TODO1: rax > r8?
+		mov rcx, [N]
+    jg print                        ; TODO1: rax > r8?
     xor rax, rax
 
     leave
@@ -28,9 +30,14 @@ main:
 
 print:
     PRINTF64 `%s\n\x0`, myString
+
+		dec rcx
+		cmp rcx, 0
+		jg print
                                     ; TODO2.2: print "Hello, World!" N times
                                     ; TODO2.1: print "Goodbye, World!"
     xor rax, rax
 
+    PRINTF64 `%s\n\x0`, byeString
     leave
     ret
