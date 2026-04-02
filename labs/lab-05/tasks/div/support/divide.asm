c39aa98f730fad9cef82a6dcb22d9aa39145f495
robertmercea <robertmercea@gmail.com>
2026-04-02 13:47:14 +0300

    lab05: divide: Add partial solution
    Added most of the solution for divide

--- labs/lab-05/tasks/div/support/divide.asm
+++ labs/lab-05/tasks/div/support/divide.asm
@@ -7,10 +7,13 @@
 section .data
     dividend1 db 91
     divisor1 db 27
+
     dividend2 dd 67254
     divisor2 dw 1349
+
     dividend3 dq 69094148
     divisor3 dq 12345678
+
     dividend4 dq 0x00000000FFFFFFFF, 0x0000000000000000
     divisor4 dq 0x0000000000010000
 
@@ -35,9 +38,33 @@ main:
     mov bl, ah
     PRINTF64 `Remainder: %hhu\n\x0`, rbx
 
-    ; TODO: Calculate quotient and remainder for dividend2 / divisor2.
+		; Quotient and remained for dividend2 / divisor2
+		xor rdx, rdx
+		xor rbx, rbx
+		mov bx, word [divisor2]
+		mov eax, [dividend2]
+		mov dx, ax
+		shr eax, 16
+		xchg dx, ax
+		mov eax, [dividend2]
+		div bx
+
+		mov bx, ax
+		PRINTF64 `Quotient: %hu\n\x0`, rbx
+
+		mov bx, dx
+		PRINTF64 `Remained: %hu\n\x0`, rbx
+
 
     ; TODO: Calculate quotient and remainder for dividend3 / divisor3.
+
+		; xor rbx, rbx
+		; mov rax, qword [dividend3]
+		; mov ebx, dword [divisor3]
+		; div ebx
+		; PRINTF64 `Quotient: %hhu\n\x0`, rax
+		; PRINTF64 `Remained: %hhu\n\x0`, rdx
+
 
     ; TODO: Calculate quotient and remainder for dividend4 / divisor4.
 
