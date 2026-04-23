d82ff5d89eaa71391599adae608945776374d86a
robertmercea <robertmercea@gmail.com>
2026-04-23 12:33:01 +0300

    lab07: max: Add solution
    Implement getting the maximum value of two registers using the stack by
    doing an extra pop only if necessary

--- labs/lab-07/tasks/max/support/max.asm
+++ labs/lab-07/tasks/max/support/max.asm
@@ -12,7 +12,16 @@ main:
     mov rax, 1
     mov rbx, 4
 
-    ; TODO: get maximum value. You are only allowed to use one conditional jump and push/pop instructions.
+		push rax
+		push rbx
+
+		cmp rax, rbx
+		jb pop_once
+		pop rax
+	
+pop_once:
+		pop rax
+
 
     PRINTF64 `Max value is: %ld\n\x0`, rax ; print maximum value
 
