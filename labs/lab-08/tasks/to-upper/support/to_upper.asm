53c7bd9e95fc73f8d9124e32e95781642e01ba5b
robertmercea <robertmercea@gmail.com>
2026-04-30 13:32:38 +0300

    lab08: to-upper: Add solution
    Implemented the to-upper function by converting each lower case
    character to its upper case counterpart

--- labs/lab-08/tasks/to-upper/support/to_upper.asm
+++ labs/lab-08/tasks/to-upper/support/to_upper.asm
@@ -2,15 +2,35 @@ section .text
 
 global to_upper
 
+; void to_upper(char *str)
 to_upper:
     push rbp
     mov rbp, rsp
 
-    ; TODO: save the used registers and align the stack, if needed
+    ; save the used registers and align the stack, if needed
+    sub rsp, 8
+    push rcx
+    push rax
 
-    ; TODO: convert the string to uppercase
+    ; convert the string to uppercase
+    xor rcx, rcx
+convert_str:
+    mov al, byte [rdi + rcx]
+    cmp al, 'a'
+    jb convert_cond
 
-    ; TODO: restore the used registers and the stack pointer, if altered
+    sub al, 0x20
+    mov byte [rdi + rcx], al
+
+convert_cond:
+    inc rcx
+    cmp byte [rdi + rcx], 0
+    jnz convert_str
+
+    ; restore the used registers and the stack pointer, if altered
+    pop rax
+    pop rcx
+    add rsp, 8
 
     leave
     ret
