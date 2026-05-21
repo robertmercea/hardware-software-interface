e286d4a37727f16b3a91625f3f788d4c670a3128
robertmercea <robertmercea@gmail.com>
2026-05-21 13:11:51 +0300

    lab11: obj-link-dev: Add solution
    Add extern references to the functions in shop.o and call them
    accordingly to set the desired values

--- labs/lab-11/tasks/obj-link-dev/support/main.c
+++ labs/lab-11/tasks/obj-link-dev/support/main.c
@@ -1,5 +1,10 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
+extern void set_price(int);
+extern int qty;
+extern void print_price(void);
+extern void print_quantity(void);
+
 int main(void)
 {
 	/*
@@ -8,6 +13,10 @@ int main(void)
 	 *    quantity is 42
 	 * without directly calling a printing function.
 	 */
+  set_price(21);
+  qty = 42;
+  print_price();
+  print_quantity();
 
 	return 0;
 }
